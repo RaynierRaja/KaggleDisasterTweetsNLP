fd10c3f828bc9229c862d4a3be8f08a972e67234
RaynierRaja <46245921+RaynierRaja@users.noreply.github.com>
2023-04-18 23:55:09 +0530

    Initial Commit

--- ExploreData.py
+++ ExploreData.py
@@ -13,3 +13,16 @@ train_df = pd.read_csv('train.csv')
 # Display 1st 10 rows
 print(train_df.head(10))
 
+
+def remove_special_char(text):
+    n = len(text)
+    formatted_text = []
+    for i in range(0, n):
+        # Remove special characters except space from the texts in each tweet
+        formatted_text.append(''.join(c for c in text[i] if (c.isalnum() or c.isspace())))
+    return formatted_text
+
+
+train_df[["text"]] = train_df[["text"]].apply(remove_special_char)
+
+print(train_df.head(10))
